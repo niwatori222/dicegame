643bc58f79a7c4c425783e83f14ed610144c0892
niwatori222 <s1320124@u-aizu.ac.jp>
2025-07-03 00:37:12 +0900

    Dice game: initial revision

--- dicegame.py
+++ dicegame.py
@@ -1,17 +1,8 @@
 import random
 
-name = input("What is your name?\n>")
-print(f"Hello,{name}!")
-
 print("Rolling dice..")
 die1 = random.randint(1, 6)
 die2 = random.randint(1, 6)
-total = die1 + die2
 print(f"Die 1: {die1}")
 print(f"Die 2: {die2}")
 print(f"Total value: {die1 + die2}")
-
-if total > 7:
-    print("You won")
-else:
-    print("You lost")

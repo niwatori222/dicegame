c8b2c740e7b4ef5de3276d89ea6aca9de14776e7
niwatori222 <s1320124@u-aizu.ac.jp>
2025-07-03 00:07:59 +0900

    Won/lost message added

--- dicegame.py
+++ dicegame.py
@@ -6,6 +6,7 @@ print(f"Hello,{name}!")
 print("Rolling dice..")
 die1 = random.randint(1, 6)
 die2 = random.randint(1, 6)
+total = die1 + die2
 print(f"Die 1: {die1}")
 print(f"Die 2: {die2}")
 print(f"Total value: {die1 + die2}")
